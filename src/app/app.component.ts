d05c7757a976794c3f55426fb364f678ccacae9f
ic3top <dvdvdv889@gmail.com>
2022-07-09 09:36:05 +0300

    Set up project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'LSI-test';
+export class AppComponent implements OnInit {
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
+
+  data: any[] = [
+    { name: 'Steave', age: '23' },
+    { name: 'Steave', age: '23' },
+    { name: 'Steave', age: '23' },
+    { name: 'Steave', age: '23' },
+    { name: 'Steave', age: '23' },
+    { name: 'Steave', age: '23' },
+    { name: 'Steave', age: '23' },
+    { name: 'Steave', age: '23' },
+    { name: 'Steave', age: '23' },
+  ];
+  cols: any[] = [
+    { header: 'Name', field: 'name' },
+    { header: 'Age', field: 'age' },
+  ];
 }
